f3898fb111f214f96432a85cf8c11b1062a39199
JanMihail <JanMihail@gmail.com>
2021-02-19 22:58:49 +0700

    My First Prototype

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System;
+
+public class MainMenuController : MonoBehaviour
+{
+    public PlayerSettings playerSettings;
+    public TMPro.TextMeshProUGUI textLength;
+
+    public void SliderLengthOnChange(float value)
+    {
+        playerSettings.Length = Convert.ToInt32(value);
+    }
+
+    public void ButtonStartOnClick()
+    {
+        
+    }
+
+    private void Update()
+    {
+        textLength.text = String.Format("LENGTH: {0}", playerSettings.Length);
+    }
+}
---
+++ Assets/Scripts/PlayerSettings.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Настройки игрока
+/// </summary>
+public class PlayerSettings : MonoBehaviour
+{
+    /// <summary>
+    /// Длина игрока
+    /// </summary>
+    [SerializeField] private int length;
+
+    public int Length
+    {
+        get => length;
+        set { length = value; }
+    }
+}
