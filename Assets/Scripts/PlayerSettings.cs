a883be314851ab73b347064c1ca51d4113d3e02e
JanMihail <JanMihail@gmail.com>
2021-02-23 01:48:07 +0700

    Новое меню

--- Assets/Scripts/PlayerSettings.cs
+++ Assets/Scripts/PlayerSettings.cs
@@ -3,10 +3,17 @@
 public class PlayerSettings : MonoBehaviour
 {
     [SerializeField] private int length;
+    [SerializeField] private bool lightOn;
 
     public int Length
     {
         get => length;
         set { length = value; }
+    }
+
+    public bool LightOn
+    {
+        get => lightOn;
+        set { lightOn = value; }
     }
 }
