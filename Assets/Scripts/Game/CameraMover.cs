247d1a231f99ae339741b67de6557a6c5af5b6d1
JanMihail <JanMihail@gmail.com>
2021-02-25 01:30:51 +0700

    Прототип уровня

---
+++ Assets/Scripts/Game/CameraMover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+    public Transform obj;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // transform.position.Set(obj.position.x, obj.position.y + 2, transform.position.z);
+
+        transform.position = new Vector3(obj.position.x, (obj.position.y + 2), transform.position.z);
+    }
+}
---
+++ Assets/Scripts/Game/FinishGame.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FinishGame : MonoBehaviour
+{
+    public Button buttonRestart;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        buttonRestart.gameObject.SetActive(true);
+    }
+}
