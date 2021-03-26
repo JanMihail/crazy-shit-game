778b4fed51c8c1245c8253b79fdef890600a1819
JanMihail <JanMihail@gmail.com>
2021-03-26 23:56:39 +0700

    Почистил проект от мусора

--- Assets/FreezeRotation.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class FreezeRotation : MonoBehaviour
-{
-    void Update()
-    {
-        transform.rotation = Quaternion.Euler(0, 0, transform.rotation.eulerAngles.z);
-    }
-}
