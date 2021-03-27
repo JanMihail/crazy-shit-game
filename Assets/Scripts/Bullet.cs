e9d8187b5c6d2592762253556a0f3689d72cfde1
JanMihail <JanMihail@gmail.com>
2021-03-27 10:27:29 +0700

    Рефакторинг тестовой сцены. Освещение.

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float lifeTimeSeconds = 20f;
+
+    void Start()
+    {
+        Destroy(gameObject, lifeTimeSeconds);
+    }
+}
---
+++ Assets/Scripts/BulletLauncher.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class BulletLauncher : MonoBehaviour
+{
+    public Rigidbody bulletPrefab;
+    public float force = 100.0f;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Rigidbody bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+            bullet.AddForce(transform.forward * force, ForceMode.VelocityChange);
+        }
+    }
+}
