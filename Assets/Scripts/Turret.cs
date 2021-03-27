83c600434c42281b276eb0d9d46285d91bb11ccf
JanMihail <JanMihail@gmail.com>
2021-03-27 15:05:32 +0700

    Сделал Turret независимой от UI

---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class Turret : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+    public float rotateSensitivity = 1.5f;
+
+    [Range(-90f, 90f)]
+    public float rotateAngleMin = -90f;
+
+    [Range(-90f, 90f)]
+    public float rotateAngleMax = 90f;
+
+    private float leftRightAngle = 0.0f;
+    private float upDownAngle = 0.0f;
+
+    private bool isValid = false;
+
+    void Start()
+    {
+        validate();
+    }
+
+    private void validate()
+    {
+        isValid = false;
+
+        if (rotateAngleMin > rotateAngleMax)
+        {
+            
+            throw new System.Exception(string.Format("rotateAngleMin {0:F2} > rotateAngleMax {1:F2}", rotateAngleMin, rotateAngleMax));
+        }
+
+        isValid = true;
+    }
+
+    public void move(float horizontal, float vertical)
+    {
+        move(horizontal, vertical, Time.deltaTime);
+    }
+
+    public void move(float horizontal, float vertical, float multiplicity)
+    {
+        if (!isValid) { return; }
+
+        float hor = horizontal * moveSpeed;
+        float vert = vertical * moveSpeed;
+
+        Vector3 direction = new Vector3(hor, 0, vert);
+
+        transform.Translate(direction.normalized * moveSpeed * multiplicity);
+    }
+
+    public void rotate(float horizontal, float vertical)
+    {
+        if (!isValid) { return; }
+
+        leftRightAngle += horizontal * rotateSensitivity;
+        upDownAngle -= vertical * rotateSensitivity;
+
+        leftRightAngle %= 360.0f;
+        upDownAngle = Mathf.Clamp(upDownAngle, rotateAngleMin, rotateAngleMax);
+
+        transform.eulerAngles = new Vector3(upDownAngle, leftRightAngle);
+    }
+}
---
+++ Assets/Scripts/TurretController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class TurretController : MonoBehaviour
+{
+    public Turret turret;
+
+    private float moveHor;
+    private float moveVert;
+    private float rotHor;
+    private float rotVert;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        updateCoords();
+    }
+
+    void Update()
+    {
+        updateCoords();
+        turret.move(moveHor, moveVert);
+        turret.rotate(rotHor, rotVert);
+    }
+
+    private void updateCoords()
+    {
+        moveHor = Input.GetAxis("WeaponHorizontal");
+        moveVert = Input.GetAxis("WeaponVertical");
+
+        rotHor = Input.GetAxis("Mouse X");
+        rotVert = Input.GetAxis("Mouse Y");
+    }
+}
